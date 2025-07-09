194451715f1716978147d048a9625aa8ee1a65e6
=saamya <=saamyasingla24@gmail.com>
2025-07-09 09:51:57 +0530

    fix L1 push

---
+++ L-01-Intro/script.js
@@ -0,0 +1,80 @@
+// console.log("hello");
+// var a = 123;
+// console.log(a);
+// function abc(){
+//     console.log("thus is sam");
+//     function hgf(){
+//         console.log("this is  function hgf ");
+//     }
+//     hgf();
+// }
+
+// function def(){
+//     console.log("THIS IS A FUNCTION DEF");
+// }
+// abc();
+// //xyz();
+// var xyz = () =>{
+//     console.log("XYZ IS AN ARROW FUNCTION");
+// }
+// xyz();
+// var b = 300;
+// console.log(b);
+
+// {
+//     //compound statement 
+//    let a = 100;
+//     let b = 200;
+//     let c = 300;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//   //  console.log(x);
+//     {
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//         let d = 200;
+//         {
+//             let  x = 2000;
+//             console.log(a);
+//             console.log(b);
+//             console.log(c);
+//             console.log(d);
+//         }
+//     }
+// }
+//function abc(){
+//     var a = 100;
+//     var b = 200;
+//     var c = 300;
+//     function def (){
+//         var d= 400;
+//         function ijk(){
+//             console.log(a);
+//              console.log(b);
+//               console.log(c);
+//                console.log(d);
+//         }
+//         ijk();
+//     }
+//     def();
+// }
+// abc();
+// if var is used then the final value of i saved globally will be printed if let then local
+// function abc()
+// {
+// for(let i = 0; i<=5 ; i++){
+
+//     setTimeout(()=>{
+//         console.log(i)
+//     },2000);
+//     // console.log(i);
+// }
+// }
+//closure - is a combination of function bundelled together with ref to surrounding  state 
+// advantage = data hiding and encapuslation
+// disadvantage closure hold memory and space , consuption of memory , not garbage collected 
+// function abc (sum){
+//     return
+// }
